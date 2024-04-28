5e16129a81fa8f718e7812df8f7d9ed6fb185951
Paulo <pauloecomercial367@gmail.com>
2024-04-28 12:10:07 -0300

    :sparkles: Concluindo tarefa

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
-from todo.views import TarefaViewSet
+from todo.views import TarefaViewSet, ConcluirTarefa
 
 router = routers.DefaultRouter()
 router.register('tarefas', TarefaViewSet,basename='tarefas')
 urlpatterns = [
     path('', include(router.urls)),
+    path('tarefas/<int:id>', ConcluirTarefa.as_view())
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,23 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from todo.models import Tarefa
+from rest_framework.response import Response
 from todo.serializers import TarefaSerializer
 
 class TarefaViewSet(viewsets.ModelViewSet):
     queryset = Tarefa.objects.all()
     serializer_class = TarefaSerializer
+    
+class ConcluirTarefa(generics.ListAPIView):
+    queryset = Tarefa.objects.all()
+    serializer_class = TarefaSerializer
+    
+    def patch(self,request, *args, **kwargs):
+        tarefa = Tarefa.objects.filter(id=kwargs['id']).first()
+        if tarefa is None:
+            return Response({'error': 'Tarefa não encontrada'}, status=404)
+        
+        tarefa.concluida = not tarefa.concluida
+        tarefa.save()
+        
+        serializer = self.get_serializer(tarefa)
+        return Response(serializer.data)
